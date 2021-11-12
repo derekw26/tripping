ef4cdfd46ff1b00079fc5587043b1348f8b70e5c
Derek Wei <derek.wei94@gmail.com>
2021-11-12 15:33:53 +1100

    google maps first display

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import Tripping from './Tripping'
+import Map from './Map'
+
 
 function App() {
   return (
     <div className="App">
       <h1>Tripping</h1>
       <Tripping />
+      <Map />
     </div>
   );
 }
