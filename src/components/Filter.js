3a92662540d98901e9304c28cf92b31373b3f8ee
Derek Wei <derek.wei94@gmail.com>
2021-11-18 20:51:15 +1100

    add

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -37,8 +37,6 @@ export default function Filter(props) {
   }
 
   const routesNoSpaces = cleanRoutes().map((route) => route.replace(/\s/g, ''));
-  console.log(routesNoSpaces)
-
 
   return (
     <List className="filter" dense sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
